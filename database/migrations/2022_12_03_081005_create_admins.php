becb8e1479fc43b299200ef43c2d3dcf45efe33d
humera fatima <faizqaimkhani@gmail.com>
2022-12-05 18:56:34 +0500

    12/5/2022 updatedbyhumera

--- database/migrations/2022_12_03_081005_create_admins.php
+++ database/migrations/2022_12_03_081005_create_admins.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string("fcm_token")->nullable();
             $table->string('photo')->nullable();
-            $table->integer('roleId');
+            $table->integer('roleId')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+
+Route::get('/', 'App\Http\Controllers\HomeController@redirectAdmin')->name('index');
+Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home');
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function () {
+
+    Route::get('/dashboard', 'App\Http\Controllers\Backend\DashboardController@index')->name('admin.dashboard');
+    Route::resource('roles', 'App\Http\Controllers\Backend\RolesController', ['names' => 'admin.roles']);
+    Route::resource('users', 'App\Http\Controllers\Backend\UsersController', ['names' => 'admin.users']);
+    Route::resource('admins', 'App\Http\Controllers\Backend\AdminsController', ['names' => 'admin.admins']);
+
+
+    // Login Routes
+    Route::get('/login', 'App\Http\Controllers\Backend\Auth\LoginController@showLoginForm')->name('admin.login');
+    Route::post('/login/submit', 'App\Http\Controllers\Backend\Auth\LoginController@login')->name('admin.login.submit');
+
+    // Logout Routes
+    Route::post('/logout/submit', 'App\Http\Controllers\Backend\Auth\LoginController@logout')->name('admin.logout.submit');
+
+    // Forget Password Routes
+    Route::get('/password/reset', 'App\Http\Controllers\Backend\Auth\ForgetPasswordController@showLinkRequestForm')->name('admin.password.request');
+    Route::post('/password/reset/submit', 'App\Http\Controllers\Backend\Auth\ForgetPasswordController@reset')->name('admin.password.update');
+
+   Route::get('/basic-controls', 'Admin\BasicController@index')->name('basic-controls');
+        Route::post('/basic-controls', 'Admin\BasicController@updateConfigure')->name('basic-controls.update');
+
+});
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
